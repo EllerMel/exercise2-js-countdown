9812d772f1f7a973796859c5befb8059a1b0c51e
M Eller <msmaedev@gmail.com>
2019-03-10 16:09:39 -0400

    Countdown total redo

--- web/js/main.js
+++ web/js/main.js
@@ -1,30 +1,46 @@
+var secondsCountDown = 0;
+var minutes = 0;
+var seconds = 0;
+var display = document.getElementById('time');
+var timer;
+var currentTime = 0;
 
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-        var x = setInterval(function () {
-            minutes = parseInt(timer / 60, 10)
-            seconds = parseInt(timer % 60, 10);
-    
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            display.textContent = minutes + ":" + seconds;
-    
-            if (--timer < 0) {
-                timer = 0;
-                var audio = new Audio("RobotTone.wav");
-                    audio.play();
-                    clearInterval(x); 
-            }
-        }, 1000);
+function inputTime() {
+    secondsCountDown = prompt("Please enter a countdown amount in seconds", "");
+    currentTime = secondsCountDown;
+    timer = setInterval(tickTimer, 1000);
+    tickTimer();
+};
+
+function tickTimer() {
+    if (currentTime < 60) {
+        countdown = "00:" + getSeconds(currentTime);
+        console.log(countdown);
+        display.innerHTML = countdown;
     }
-    
-    window.onload = function () {
-        var secondsCountDown = prompt("Please enter a countdown amount in seconds", "");
-            display = document.querySelector('#time');
-        startTimer(secondsCountDown, display);
-    };
+    else {
+        countdown = getMinutes(currentTime) + ":" + getSeconds(currentTime);
+        console.log(countdown);
+        display.innerHTML = countdown;
+    }
+    if (currentTime == 0) {
+        clearInterval(timer);
+    }
+    currentTime--;
+}
 
+function getMinutes(time) {
+    minutes = Math.floor(time / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    return minutes;
+}
 
-
-
+function getSeconds(time) {
+    seconds = time % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    return seconds;
+}
